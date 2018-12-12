935914910e86a478c24165bb787c1a3a26484598
JAY <johnjohnsonokah@gmail.com>
2018-12-12 14:14:13 +0100

    a constructor's prototype

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -3,9 +3,13 @@
     var App = window.App || {};
 
     function DataStore() {
-        console.log('running the DataStore function');
         this.data = {}
     }
+    
+    DataStore.prototype.add = function (key, val) {
+        this.data[key] = val;
+    }
+
     App.DataStore = DataStore;
     window.App = App;
 })(window);
